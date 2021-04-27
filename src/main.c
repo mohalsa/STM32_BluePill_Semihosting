9b26d0c63d5716ecc97b29b79401451d98f749de
mohalsa <engmoh907@gmail..com>
2021-04-28 01:09:03 +0300

    Minor changes

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
     1- Open Command Palette by holding "CMD + SHIFT + P" in Mac OS machines and "CTRL + SHIFT + P" or just click on "View" then "Command Palette...".
     2- Type "Tasks: Run Task" without the double qoutation marks and select it by pressing Enter.
     3- Select "OpenOCD (Allow semihosting output)".
+    3.1- This Task Should run once when loading the project. If you try to run it again you'll get "The task 'OpenOCD (Allow semihosting output) (0-SemiHosting_Configurations)' is already active." message
     4- A terminal view will indicate a successfull connection with a final message of "Info : Listening on port 3333 for gdb connections".
     5- Click on the "Run and Debug" Icon (Left Side) or SHIFT+CMD+D (Mac OS) or SHIFT+CTRL+D (Windows OS)
     6- Click "Start Debugging" the green arrow (Top left), next to PIO Debug.

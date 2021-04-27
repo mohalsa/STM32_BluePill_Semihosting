fc2f019954251371a030525d0d7ce4b9ea0e0fc0
mohalsa <engmoh907@gmail..com>
2021-04-28 01:06:08 +0300

    Added detailed info

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/*
+    ARM Cortex M3 STM32 Bluepill Semihosting-Enabled Project by Mohammed Alsada
+    GitHub: https://github.com/mohalsa
+
+    Please follow the instructions carefully!
+    First Connect Your STlink debugger to USB and to the bluepill board (Drivers must be installed before, if needed.).
+    Then:
+    1- Open Command Palette by holding "CMD + SHIFT + P" in Mac OS machines and "CTRL + SHIFT + P" or just click on "View" then "Command Palette...".
+    2- Type "Tasks: Run Task" without the double qoutation marks and select it by pressing Enter.
+    3- Select "OpenOCD (Allow semihosting output)".
+    4- A terminal view will indicate a successfull connection with a final message of "Info : Listening on port 3333 for gdb connections".
+    5- Click on the "Run and Debug" Icon (Left Side) or SHIFT+CMD+D (Mac OS) or SHIFT+CTRL+D (Windows OS)
+    6- Click "Start Debugging" the green arrow (Top left), next to PIO Debug.
+    7- Debugging session will start.
+    8- In Terminal window, press any key to show the debugging info + the printf messages.
+
+*/
+
 #include <stm32f1xx.h>
 #include <stdio.h>
 
@@ -8,5 +26,5 @@ int main(void)
 {
     initialise_monitor_handles();   /* This Function MUST come before the first printf() */
     printf("Hello, world!\n");
-    fflush(stdout);
+    fflush(stdout);                 /* Needed to display the printf message */
 }
